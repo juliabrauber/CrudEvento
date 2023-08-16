19f3f9bd2db4e8ca2ea85606ffe9346ce78c3671
Julia Rauber <juliabrauber@gmail.com>
2023-08-16 11:30:15 -0300

    16082023

---
+++ Back/src/ProEventos.Application/Contratos/IEventosService.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProEventos.Domain;
+using ProEventos.Persistence;
+
+namespace ProEventos.Application.Contratos
+{
+    public interface IEventoService
+    {
+        Task<Evento> AddEventos(Evento Model);
+        Task<Evento> UpdateEvento(int eventoId, Evento Model);
+        Task<bool> DeleteEvento(int eventoId);
+
+         //EVENTOS
+         Task<Evento[]> GetAllEventosAsync(bool includePalestrantes = false);
+        Task<Evento[]> GetAllEventosByTemaAsync(string tema, bool includePalestrantes = false);
+        Task<Evento> GetEventoByIdAsync(int EventoId, bool includePalestrantes = false);
+    }
+}
