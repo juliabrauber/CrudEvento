ec0b6d5ad6fe20db30df8bde313a19ce7dfb76b8
Julia Rauber <juliabrauber@gmail.com>
2023-09-19 16:23:34 -0300

    19092023

--- Front/Projeto-App/src/app/shared/nav/nav.component.ts
+++ Front/Projeto-App/src/app/shared/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-Nav',
@@ -7,12 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
   isCollapsed = true;
-  constructor() {
-
-
+  constructor( private router: Router) {
    }
 
-  ngOnInit() {
+  ngOnInit(): void {
+  }
+
+  showMenu( ): boolean{
+    return this.router.url !== '/user/login';
   }
 
 }
